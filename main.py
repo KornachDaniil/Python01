613488c82e704b9ea76aa3f137329969dcaf0f24
Kornach Daniil <danik.kornach@gmail.com>
2024-02-11 09:58:40 +0300

    Произведена оптимизаия кода.

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+def For(i, attempts, iterator, n, start):
+    last_number = 0
+    for j in range(i, attempts):
+        import random
+        random_number = random.randint(start, n)
+        last_number = random_number
+        if last_number == random_number:
+            random_number = random.randint(start, n)
+            last_number = random_number
+        print("Это", last_number, "?")
+        answer = input()
+        iterator = iterator + 1
+        if answer == "y" or answer == "Y":
+            print("Ура!")
+            return
+        if iterator == attempts:
+            print("Попытки кончились")
+            return
+
+
 def Find():
-    n = 50
-    start = 0
     attempts = 7
     iterator = 0
     for i in range(1, attempts):
-        print("Число <", n, "(Y - да, N - нет):")
+        print("Число <", 50, "(Y - да, N - нет):")
         answer = input()
         iterator = iterator + 1
         if answer == "y" or answer == "Y":
@@ -12,68 +30,21 @@ def Find():
             answer = input()
             iterator = iterator + 1
             if answer == "y" or answer == "Y":
-                n = 25
-                for j in range(i, attempts):
-                    import random
-                    random_number = random.randint(start, n)
-                    print(random_number)
-                    answer = input("Это:")
-                    iterator = iterator + 1
-                    if answer == "y" or answer == "Y":
-                        print("Ура!")
-                        return
-                    if iterator == attempts:
-                        print("Попытки кончились")
-                        return
+                For(i, attempts, iterator, n=25, start=0)
+                return
             else:
-                start = 25
-                n = 50
-                for k in range(i, attempts):
-                    import random
-                    random_number = random.randint(start, n)
-                    print(random_number)
-                    answer = input("Это:")
-                    iterator = iterator + 1
-                    if answer == "y" or answer == "Y":
-                        print("Ура!")
-                        return
-                    if iterator == attempts:
-                        print("Попытки кончились")
-                        return
+                For(i, attempts, iterator, n=50, start=25)
+                return
         else:
             print("Число <", 75, "(Y - да, N - нет):")
             answer = input()
             iterator = iterator + 1
             if answer == "y" or answer == "Y":
-                start = n
-                n = 75
-                for g in range(i, attempts):
-                    import random
-                    random_number = random.randint(start, n)
-                    print(random_number)
-                    answer = input("Это:")
-                    iterator = iterator + 1
-                    if answer == "y" or answer == "Y":
-                        print("Ура!")
-                        return
-                    if iterator == attempts:
-                        print("Попытки кончились")
-                        return
+                For(i, attempts, iterator, n=75, start=50)
+                return
             else:
-                start = 75
-                n = 100
-                for h in range(i, attempts):
-                    import random
-                    random_number = random.randint(start, n)
-                    print(random_number)
-                    answer = input("Это:")
-                    iterator = iterator + 1
-                    if answer == "y" or answer == "Y":
-                        print("Ура!")
-                        return
-                    if iterator == attempts:
-                        print("Попытки кончились")
-                        return
+                For(i, attempts, iterator, n=100, start=75)
+                return
     if iterator == attempts:
         print("Попытки кончились")
         return
